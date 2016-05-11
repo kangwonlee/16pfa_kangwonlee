7129d175d9c1872aabf76396e50933f9fa55d58b
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-11 20:11:59 +0900

    project Euler 0001

--- euler/euler_0001.py
+++ euler/euler_0001.py
@@ -6,6 +6,14 @@
 1000보다 작은 자연수 중에서 3 또는 5의 배수를 모두 더하면 얼마일까요?
 """
 
+result = 0
+
+for i in range(1, 1000):
+    if ((i % 3) == 0) or ((i % 5) == 0):
+        result += i
+
+print ("result = %d" % result)
+
 # 프로그램 작성 후 add, commit
 
 # 각 행 주석 입력 후 commit
