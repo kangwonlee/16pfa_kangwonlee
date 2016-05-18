6b7664c7cd63a987aab6a9c17c0925fb3e7f8dc5
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-18 20:35:09 +0900

    Project Euler 0001.9 start

---
+++ euler/euler_00019.py
@@ -0,0 +1,25 @@
+# -*-coding:utf8
+# http://euler.synap.co.kr/prob_detail.php?id=2
+"""
+피보나치 수열의 각 항은 바로 앞의 항 두 개를 더한 것이 됩니다. 1과 2로 시작하는 경우 이 수열은 아래와 같습니다.
+    1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+짝수이면서 4백만 이하인 모든 항을 더하면 얼마가 됩니까?
+
+대신
+
+짝수이면서 4백만 이하인 가장 큰 항은 얼마가 됩니까?
+
+* Recursion 재귀함수 를 이용해 볼 수 있겠습니까?
+def factorial(n):
+    if 1 == n:
+        return 1
+else:
+        return n * factorial(n - 1)
+
+print(factorial(5))
+"""
+
+
+# 프로그램 작성 후 add, commit
+
+# 각 행 주석 입력 후 commit
