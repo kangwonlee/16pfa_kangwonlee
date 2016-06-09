dfac894fb3e23da6d1ab6372a8184cd69b8bb228
KangWon LEE <kangwon_fwd@naver.com>
2016-06-09 13:08:31 +0900

    is_python_3_compatible.py : added process_folder() : change to folder, list contents, if .py, print the name

--- is_python_3_compatible.py
+++ is_python_3_compatible.py
@@ -1,4 +1,21 @@
 import os
+import subprocess
+
+
+def process_folder(folder_name):
+    # store current folder to return
+    current_folder = os.path.abspath(os.curdir)
+
+    # change to the given folder
+    os.chdir(folder_name)
+
+    dir_list = os.listdir(os.curdir)
+    for name in dir_list:
+        if os.path.isfile(name) and '.py' == os.path.splitext(name)[-1]:
+            print(name)
+
+    # return to stored folder
+    os.chdir(current_folder)
 
 
 def main():
@@ -6,7 +23,7 @@ def main():
     for name in dir_list:
         if os.path.isdir(name) and 'ex' == name[:2]:
             print(name)
-
+            process_folder(name)
 
 if __name__ == '__main__':
     main()
