349aa2015cd779e19c719f827d1fb3bedcc35825
KangWon LEE <kangwon_fwd@naver.com>
2016-07-15 22:35:04 +0900

    is_python_3_compatible.py : added process_py_file() : try to run using commandline     added python() : return full path to python3 as a script argument

--- is_python_3_compatible.py
+++ is_python_3_compatible.py
@@ -1,5 +1,22 @@
+# coding: utf8
 import os
 import subprocess
+
+
+def python3():
+    from sys import argv
+    arg0 = argv[1]
+
+    return arg0
+
+
+def process_py_file(filename):
+    try:
+        subprocess.call('%s %s' % (python3(), filename), shell=True)
+    except:
+        print("%s failed" % filename)
+    else:
+        print("success %s" % filename)
 
 
 def process_folder(folder_name):
@@ -12,7 +29,8 @@ def process_folder(folder_name):
     dir_list = os.listdir(os.curdir)
     for name in dir_list:
         if os.path.isfile(name) and '.py' == os.path.splitext(name)[-1]:
-            print(name)
+            print('file %s' % name)
+            process_py_file(name)
 
     # return to stored folder
     os.chdir(current_folder)
@@ -22,7 +40,7 @@ def main():
     dir_list = os.listdir(os.curdir)
     for name in dir_list:
         if os.path.isdir(name) and 'ex' == name[:2]:
-            print(name)
+            print('folder %s' % name)
             process_folder(name)
 
 if __name__ == '__main__':
