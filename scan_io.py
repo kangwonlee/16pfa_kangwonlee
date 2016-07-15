1102d662a14a38db1a5044dd28b102432cc9523a
KangWon LEE <kangwon_fwd@naver.com>
2016-07-15 22:46:58 +0900

    added scan_io.py : which file needs inputs? raw_input or argv ex11.py needs input ex12.py needs input ex13.py needs argv ex14.py needs input ex14.py needs argv ex15.py needs input ex15.py needs argv ex16.py needs input ex16.py needs argv ex17.py needs argv ex20.py needs argv ex31.py needs input ex35.py needs input ex41.py needs input

---
+++ scan_io.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*-
+import os
+import subprocess
+from is_python_3_compatible import python3
+
+
+def process_py_file(filename):
+    try:
+        f = open(filename, 'r', encoding="utf-8")
+        txt = f.read()
+        f.close()
+    except UnicodeDecodeError:
+        f = open(filename, 'r', encoding="cp949")
+        txt = f.read()
+        f.close()
+    except TypeError as e:
+        # 'encoding' is an invalid keyword argument for this function
+        f = open(filename, 'r')
+        txt = f.read()
+        f.close()
+
+    patterns = {'argv': 'import argv', 'input': 'raw_input'}
+
+    for key, signature in patterns.items():
+        if signature in txt:
+            print("%s needs %s" % (filename, key))
+
+
+def process_folder(folder_name):
+    # store current folder to return
+    current_folder = os.path.abspath(os.curdir)
+
+    # change to the given folder
+    os.chdir(folder_name)
+
+    dir_list = os.listdir(os.curdir)
+    for name in dir_list:
+        if os.path.isfile(name) and '.py' == os.path.splitext(name)[-1]:
+            # print('file %s' % name)
+            process_py_file(name)
+
+    # return to stored folder
+    os.chdir(current_folder)
+
+
+def main():
+    dir_list = os.listdir(os.curdir)
+    for name in dir_list:
+        if os.path.isdir(name) and 'ex' == name[:2]:
+            # print('folder %s' % name)
+            process_folder(name)
+
+
+if __name__ == '__main__':
+    main()
