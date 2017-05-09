a91cd5a6364c849b3b24e0ebb07fdce26408593b
KangWon Lee <kangwon.lee@kpu.ac.kr>
2017-05-09 23:52:41 +0900

    refactored process_python_script()

--- tests/test_ex01.py
+++ tests/test_ex01.py
@@ -16,18 +16,21 @@ def test_all_scripts_recursively():
         if '.git' not in full_path:
             for file_name in file_list:
                 if file_name.endswith('.py'):
-                    script_full_path = os.path.join(full_path, file_name)
-                    print(script_full_path)
-                    with open(script_full_path, 'rt', encoding='utf8') as input_file:
-                        script_text = input_file.read()
+                    process_python_script(file_name, full_path)
 
-                    # skip if
-                    if 'argv' not in script_text:
 
-                        completed_process = subprocess.run(['python', script_full_path], stdout=subprocess.PIPE)
-                        nt.assert_equal(0, completed_process.returncode, msg=script_full_path)
-                        # true if script output is not empty or print is not in script
-                        nt.assert_true(completed_process.stdout or ('print' not in script_text), msg=script_full_path)
+def process_python_script(file_name, full_path):
+    script_full_path = os.path.join(full_path, file_name)
+    print(script_full_path)
+    with open(script_full_path, 'rt', encoding='utf8') as input_file:
+        script_text = input_file.read()
+
+    # skip if
+    if 'argv' not in script_text:
+        completed_process = subprocess.run(['python', script_full_path], stdout=subprocess.PIPE)
+        nt.assert_equal(0, completed_process.returncode, msg=script_full_path)
+        # true if script output is not empty or print is not in script
+        nt.assert_true(completed_process.stdout or ('print' not in script_text), msg=script_full_path)
 
 
 if __name__ == '__main__':
