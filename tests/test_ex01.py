f8c5543de7d7ebdb65d2b90c5221b6c887312f31
KangWon Lee <kangwon.lee@kpu.ac.kr>
2017-05-09 23:16:31 +0900

    both tests would succeed

--- tests/test_ex01.py
+++ tests/test_ex01.py
@@ -2,17 +2,18 @@ import os
 import subprocess
 
 import nose
-from nose.tools import *
+import nose.tools as nt
 
 
 def test_ex01_success():
     completed_process = subprocess.run(['python', os.path.join('ex01_print', 'ex01.py')], stdout=subprocess.PIPE)
-    assert_true(completed_process)
+    nt.assert_equal(0, completed_process.returncode)
+    nt.assert_true(completed_process.stdout)
 
 
-def test_ex01_fail():
+def test_ex01_error():
     completed_process = subprocess.run(['python', os.path.join('ex01_print', 'ex01_error.py')], stdout=subprocess.PIPE)
-    assert_false(completed_process)
+    nt.assert_not_equal(0, completed_process.returncode)
 
 
 if __name__ == '__main__':
